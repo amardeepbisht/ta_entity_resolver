28208f94673df3ef21a1c7d418196a26428ce4a4
amardeepbisht <amar.bisht29@gmail.com>
2025-06-08 16:47:23 +0530

    Updated prject structure to include add ta_entity_resolver as root directory and putting all existing file inside /ta_entity_resolver/ directory. This is to make the project structure aligned for wheel file.

--- setup.py
+++ setup.py
@@ -1,13 +1,4 @@
-# setup.py
 from setuptools import setup, find_packages
-
-def load_requirements(path="requirements.txt"):
-    with open(path, encoding="utf-8") as f:
-        # strip out any comments or blank lines
-        return [
-            line.strip() for line in f
-            if line.strip() and not line.startswith("#")
-        ]
 
 setup(
     name="ta_entity_resolver",
@@ -15,7 +6,18 @@ setup(
     description="Modular ER engine for Databricks",
     packages=find_packages(),
     include_package_data=True,
-    install_requires=load_requirements(),    # ← loads every line in requirements.txt
+    install_requires=[
+        "PyYAML>=6.0",
+        "thefuzz",
+        "RapidFuzz",
+        "langchain",
+        "neo4j",
+        "openai",
+        "Spark-Matcher",
+        "scikit-learn",
+        "pandas",
+        "pyarrow",
+    ],
     entry_points={
         "console_scripts": [
             "ta-er=main:main",
